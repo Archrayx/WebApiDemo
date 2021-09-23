da934da21bfb7b56e96960f8b55ff9ded8e15f1e
river <river@CONNECT>
2021-09-22 22:35:01 -0400

    mAdd project files.

---
+++ Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+using Microsoft.Owin;
+using WebApiDemo.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+
+[assembly: OwinStartup(typeof(WebApiDemo.Startup))]
+
+namespace WebApiDemo
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+        }
+
+        private void ConfigureOAuth(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<DBContext>(() => new DBContext());
+            app.CreatePerOwinContext<UserManager<IdentityUser>>(CreateManager);
+        }
+
+        private static UserManager<IdentityUser> CreateManager(IdentityFactoryOptions<UserManager<IdentityUser>> options, IOwinContext context)
+        {
+            var userStore = new UserStore<IdentityUser>(context.Get<DBContext>());
+            var owinManager = new UserManager<IdentityUser>(userStore);
+            return owinManager;
+        }
+    }
+}
+https://www.c-sharpcorner.com/UploadFile/ff2f08/token-based-authentication-using-Asp-Net-web-api-owin-and-i/
